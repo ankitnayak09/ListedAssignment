e2001148ab2f93ea0ab1352feb92f18b60eaf381
Ankit Kumar Nayak <68335227+ankitnayak09@users.noreply.github.com>
2023-04-20 16:59:25 +0530

    Update Env Variable

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 var express = require("express");
 var cors = require("cors");
 var indexRouter = require("./routes/index");
-
+var dotenv = require("dotenv");
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
+
+app.use(dotenv.config());
 
 app.use("/", indexRouter);
 
